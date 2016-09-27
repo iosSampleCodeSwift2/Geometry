833077538643737eca1964d5903472720481f913
김대섭 <daesub0515@naver.com>
2016-09-27 10:14:57 +0900

    Initial Commit

---
+++ Geometry/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Geometry
+//
+//  Created by Daesub Kim on 2016. 9. 27..
+//  Copyright © 2016년 Daesub Kim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
